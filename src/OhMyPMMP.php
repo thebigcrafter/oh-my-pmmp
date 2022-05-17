1bdbb99a20f912f1da4cea91b624b2faab31c764
MintoD <tobythedev7@outlook.com>
2022-05-17 16:20:50 +0700

    Create own Singleton trait

--- src/OhMyPMMP.php
+++ src/OhMyPMMP.php
@@ -4,37 +4,33 @@ declare(strict_types=1);
 
 namespace thebigcrafter\OhMyPMMP;
 
+use pocketmine\plugin\PluginBase;
 use thebigcrafter\OhMyPMMP\commands\InstallPluginCommand;
 use thebigcrafter\OhMyPMMP\commands\RemovePluginCommand;
 use thebigcrafter\OhMyPMMP\commands\UpgradePluginCommand;
 use thebigcrafter\OhMyPMMP\tasks\CachePoggitPlugins;
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\SingletonTrait;
+use thebigcrafter\OhMyPMMP\utils\SingletonTrait;
 
 class OhMyPMMP extends PluginBase {
-    use SingletonTrait;
+	use SingletonTrait;
 
-    public array $pluginsList = [];
+	public array $pluginsList = [];
 
-    public function onEnable(): void {
-        $this->setInstance($this);
+	public function onEnable(): void {
+		$this->setInstance($this);
 
-        $this->getServer()->getAsyncPool()->submitTask(new CachePoggitPlugins());
+		$this->getServer()->getAsyncPool()->submitTask(new CachePoggitPlugins());
 
-        $this->getServer()->getCommandMap()->register("oh-my-pmmp", new InstallPluginCommand());
-        $this->getServer()->getCommandMap()->register("oh-my-pmmp", new RemovePluginCommand());
-        $this->getServer()->getCommandMap()->register("oh-my-pmmp", new UpgradePluginCommand());
-    }
+		$this->getServer()->getCommandMap()->register("oh-my-pmmp", new InstallPluginCommand());
+		$this->getServer()->getCommandMap()->register("oh-my-pmmp", new RemovePluginCommand());
+		$this->getServer()->getCommandMap()->register("oh-my-pmmp", new UpgradePluginCommand());
+	}
 
-    public function setPluginsList(array $pluginsList): void {
-        $this->pluginsList = $pluginsList;
-    }
+	public function setPluginsList(array $pluginsList): void {
+		$this->pluginsList = $pluginsList;
+	}
 
-    public function getPluginsList(): array {
-        return $this->pluginsList;
-    }
-
-    public static function getInstance(): OhMyPMMP {
-        return self::$instance;
-    }
+	public function getPluginsList(): array {
+		return $this->pluginsList;
+	}
 }
---
+++ src/utils/SingletonTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace thebigcrafter\OhMyPMMP\utils;
+
+use thebigcrafter\OhMyPMMP\OhMyPMMP;
+
+trait SingletonTrait {
+	public static OhMyPMMP $instance;
+
+	public static function setInstance(OhMyPMMP $instance): void
+	{
+		self::$instance = $instance;
+	}
+
+	public static function getInstance(): OhMyPMMP
+	{
+		return self::$instance;
+	}
+}
