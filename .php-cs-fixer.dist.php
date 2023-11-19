8c6f2dbf18086a1538e80180ef764e72c5c10729
nmtho <144540995+toby7002@users.noreply.github.com>
2023-11-19 15:33:30 +0700

    Reset everything

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,106 +1,121 @@
 <?php
 
+/*
+ * This file is part of oh-my-pmmp.
+ *
+ * (c) thebigcrafter <hello@thebigcrafter.team>
+ *
+ * This source file is subject to the GPL-3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 declare(strict_types=1);
 
 $header = <<<'EOF'
     This file is part of oh-my-pmmp.
-    (c) thebigcrafter <hello.thebigcrafter@gmail.com>
+
+    (c) thebigcrafter <hello@thebigcrafter.team>
+
     This source file is subject to the GPL-3.0 license that is bundled
     with this source code in the file LICENSE.
     EOF;
 
-$config = new PhpCsFixer\Config();
+$finder = (new PhpCsFixer\Finder())
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->in(__DIR__)
+;
 
-return $config->setRules([
-    'align_multiline_comment' => [
-        'comment_type' => 'phpdocs_only'
-    ],
-    'array_indentation' => true,
-    'array_syntax' => [
-        'syntax' => 'short'
-    ],
-    'binary_operator_spaces' => [
-        'default' => 'single_space'
-    ],
-    'blank_line_after_namespace' => true,
-    'blank_line_after_opening_tag' => true,
-    'blank_line_before_statement' => [
-        'statements' => [
-            'declare'
-        ]
-    ],
-    'cast_spaces' => [
-        'space' => 'single'
-    ],
-    'concat_space' => [
-        'spacing' => 'one'
-    ],
-    'declare_strict_types' => true,
-    'elseif' => true,
-    'fully_qualified_strict_types' => true,
-    'global_namespace_import' => [
-        'import_constants' => true,
-        'import_functions' => true,
-        'import_classes' => null,
-    ],
-    'header_comment' => [
-        'comment_type' => 'comment',
-        'header' => $header,
-        'location' => 'after_open'
-    ],
-    'indentation_type' => true,
-    'logical_operators' => true,
-    'native_constant_invocation' => [
-        'scope' => 'namespaced'
-    ],
-    'native_function_invocation' => [
-        'scope' => 'namespaced',
-        'include' => ['@all'],
-    ],
-    'new_with_braces' => [
-        'named_class' => true,
-        'anonymous_class' => false,
-    ],
-    'no_closing_tag' => true,
-    'no_empty_phpdoc' => true,
-    'no_extra_blank_lines' => true,
-    'no_superfluous_phpdoc_tags' => [
-        'allow_mixed' => true,
-    ],
-    'no_trailing_whitespace' => true,
-    'no_trailing_whitespace_in_comment' => true,
-    'no_whitespace_in_blank_line' => true,
-    'no_unused_imports' => true,
-    'ordered_imports' => [
-        'imports_order' => [
-            'class',
-            'function',
-            'const',
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'align_multiline_comment' => [
+            'comment_type' => 'phpdocs_only'
         ],
-        'sort_algorithm' => 'alpha'
-    ],
-    'phpdoc_align' => [
-        'align' => 'vertical',
-        'tags' => [
-            'param',
-        ]
-    ],
-    'phpdoc_line_span' => [
-        'property' => 'single',
-        'method' => null,
-        'const' => null
-    ],
-    'phpdoc_trim' => true,
-    'phpdoc_trim_consecutive_blank_line_separation' => true,
-    'return_type_declaration' => [
-        'space_before' => 'one'
-    ],
-    'single_blank_line_at_eof' => true,
-    'single_import_per_statement' => true,
-    'strict_param' => true,
-    'unary_operator_spaces' => true,
-])
-    ->setFinder(PhpCsFixer\Finder::create()->in(__DIR__ . "/src"))
-    ->setIndent("\t")
-    ->setLineEnding("\n")
-    ->setRiskyAllowed(true);
+        'array_indentation' => true,
+        'array_syntax' => [
+            'syntax' => 'short'
+        ],
+        'binary_operator_spaces' => [
+            'default' => 'single_space'
+        ],
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'blank_line_before_statement' => [
+            'statements' => [
+                'declare'
+            ]
+        ],
+        'cast_spaces' => [
+            'space' => 'single'
+        ],
+        'concat_space' => [
+            'spacing' => 'one'
+        ],
+        'declare_strict_types' => true,
+        'elseif' => true,
+        'fully_qualified_strict_types' => true,
+        'global_namespace_import' => [
+            'import_constants' => true,
+            'import_functions' => true,
+            'import_classes' => null,
+        ],
+        'header_comment' => [
+            'comment_type' => 'comment',
+            'header' => $header,
+            'location' => 'after_open'
+        ],
+        'indentation_type' => true,
+        'logical_operators' => true,
+        'native_constant_invocation' => [
+            'scope' => 'namespaced'
+        ],
+        'native_function_invocation' => [
+            'scope' => 'namespaced',
+            'include' => ['@all'],
+        ],
+        'new_with_braces' => [
+            'named_class' => true,
+            'anonymous_class' => false,
+        ],
+        'no_closing_tag' => true,
+        'no_empty_phpdoc' => true,
+        'no_extra_blank_lines' => true,
+        'no_superfluous_phpdoc_tags' => [
+            'allow_mixed' => true,
+        ],
+        'no_trailing_whitespace' => true,
+        'no_trailing_whitespace_in_comment' => true,
+        'no_whitespace_in_blank_line' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha'
+        ],
+        'phpdoc_align' => [
+            'align' => 'vertical',
+            'tags' => [
+                'param',
+            ]
+        ],
+        'phpdoc_line_span' => [
+            'property' => 'single',
+            'method' => null,
+            'const' => null
+        ],
+        'phpdoc_trim' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'return_type_declaration' => [
+            'space_before' => 'one'
+        ],
+        'single_blank_line_at_eof' => true,
+        'single_import_per_statement' => true,
+        'strict_param' => true,
+        'unary_operator_spaces' => true
+    ])
+    ->setFinder($finder)
+;
---
+++ src/OhMyPMMP.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of oh-my-pmmp.
+ *
+ * (c) thebigcrafter <hello@thebigcrafter.team>
+ *
+ * This source file is subject to the GPL-3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace thebigcrafter\omp;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use pocketmine\plugin\PluginBase;
+
+class OhMyPMMP extends PluginBase {
+    public function onEnable() : void {}
+}
